33bbd77c969df95ec2d39070799c2fa62765ae7b
LucasBargas <lucas.bargasoficial@gmail.com>
2022-10-16 21:45:33 -0300

    Page not found - created

--- src/pages/NotFound/styles.js
+++ src/pages/NotFound/styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const NotFoundContainer = styled.section`
   h1 {
     color: ${({ theme }) => theme.colors.primaryColor};
+    font-size: 1.5rem;
   }
 `;
