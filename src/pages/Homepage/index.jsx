1a686e73d9c304dbc2d7808bc055672e9194f4fc
Lucas Bargas <76006347+LucasBargas@users.noreply.github.com>
2022-10-20 00:55:40 -0300

    Update index.jsx

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useReqApi from '../../hooks/useReqApi';
 import Showcase from '../../components/Showcase';
 import Loading from '../../components/Loading';
@@ -7,13 +7,14 @@ import Head from '../../components/Head';
 
 const Homepage = () => {
   const { datas, loading } = useReqApi('/news');
+  const [pageTitle] = useState('NewsLBS - Home');
 
-  if (loading) return <Loading title="NewsLBS" />;
+  if (loading) return <Loading title={pageTitle} />;
 
   if (!datas || datas.length === 0)
     return (
       <>
-        <Head title="NewsLBS" />
+        <Head title={pageTitle} />
         <NoNews />;
       </>
     );
@@ -21,7 +22,7 @@ const Homepage = () => {
   return (
     datas && (
       <>
-        <Head title="NewsLBS" />
+        <Head title={pageTitle} />
         <section>
           <Showcase datas={datas} accessBtn />
         </section>
